9af7bdbcc7481af7cbe99db88f7167c58001aec1
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-26 14:15:23 +0530

    Final commit : UC4 added

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,10 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 isHead=1
 isTails=0
 CountH=0
 CountT=0
-while [[ $CountH -lt 21 && $CountT -lt 21 ]]
-do
+function Play()
+{
+ while [[ $CountH -lt 21 && $CountT -lt 21 ]]
+ do
 	flip=$((RANDOM%2))
 	if [ $flip -eq $isHead ]
 	then
@@ -14,18 +16,28 @@ do
 		echo "Tails"
 		CountT=$(($CountT+1))
 	fi
-done
-
+ done
+}
+Play
 Diff=$(($CountH-$CountT))
 if [ $Diff -lt 0 ]
 then
 	Diff=$(($Diff*-1))
 	echo "Tails won by $Diff times. "
+	exit
 elif [ $Diff -gt 0 ]
 then
 	echo "Heads Won by $Diff times. "
+	exit
 else
 	echo "Tie"
+	while [[ $Diff -gt 0 && $Diff -lt 2 ]]
+	do
+	Play
+	done
+	exit
 fi
+
+
 
 
