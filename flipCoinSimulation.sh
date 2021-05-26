3a3ab2f970462b6dae1c53d0f73f133ecda36a03
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-26 12:36:39 +0530

    Added: Count of wins

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 isHead=1
 isTails=0
-flip=$((RANDOM%2))
-if [ $flip -eq $isHead ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+CountH=0
+CountT=0
+for ((i=0;i<=10;i++))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq $isHead ]
+	then
+		echo "Heads"
+		CountH=$(($CountH+1))
+	else
+		echo "Tails"
+		CountT=$(($CountT+1))
+	fi
+done
+echo "Out of 10 flips heads won $CountH times and Tails won $CountT times "
